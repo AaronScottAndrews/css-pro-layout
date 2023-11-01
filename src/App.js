45c77219ca170550a73eb54d9ba99d72a17d5cf6
Aaron Scott Andrews <AaronScottAndrews@outlook.com>
2023-11-01 13:10:57 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import React, { useState } from "react";
+import Content from "./Content";
+import "css-pro-layout/dist/css/css-pro-layout.min.css";
+import "./styles.css";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <header className="header">Header</header>
+      <div className="layout has-sidebar">
+        <aside className="sidebar">
+          Sidebar
+        </aside>
+        <div className="overlay"></div>
+        <div className="layout">
+          <main className="Content">
+            <Content/>
+          </main>
+          <footer className="footer">Footer</footer>
+        </div>
+      </div>
     </div>
   );
 }
